e3add580b510af7117e85cb5f7ad3199482d6d17
ruben <ruben_halasz12@yahoo.com>
2025-05-07 20:13:53 +0300

    Initializing the project

---
+++ src/main/java/hu/alliance/dawnguard/DawnguardApplication.java
@@ -0,0 +1,13 @@
+package hu.alliance.dawnguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawnguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawnguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/alliance/dawnguard/DawnguardApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.alliance.dawnguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawnguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
